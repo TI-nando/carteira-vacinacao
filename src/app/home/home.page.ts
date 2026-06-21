f50d10dfe21288cde8dbfb6567b96d260dab2021
TI-nando <fernandoagro.654@gmail.com>
2026-06-21 12:48:41 -0300

    Atualizando a logica e os incones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,8 +3,11 @@ import { CommonModule } from '@angular/common'; // Necessário para usar *ngIf e
 import {
   IonHeader, IonToolbar, IonTitle, IonContent,
   IonSegment, IonSegmentButton, IonLabel, IonAvatar,
-  IonCard, IonItem
+  IonCard, IonItem, IonList, IonBadge, IonIcon
 } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { checkmarkCircle, time, warning } from 'ionicons/icons';
 
 import { DataService } from '../core/services/data.service';
 import { Child } from '../core/models/child.model';
@@ -15,10 +18,10 @@ import { Child } from '../core/models/child.model';
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [
-    CommonModule, // Importante!
+    CommonModule,
     IonHeader, IonToolbar, IonTitle, IonContent,
     IonSegment, IonSegmentButton, IonLabel, IonAvatar,
-    IonCard, IonItem
+    IonCard, IonItem, IonList, IonBadge, IonIcon
   ],
 })
 export class HomePage implements OnInit {
@@ -29,13 +32,14 @@ export class HomePage implements OnInit {
   selectedChild: Child | null = null;
 
   // Injetamos o serviço de dados no construtor
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService) {
+    addIcons({ checkmarkCircle, time, warning });
+  }
 
   // Esse método roda automaticamente assim que a página é carregada
   ngOnInit() {
     this.dataService.getChildren().subscribe((data) => {
       this.children = data;
-      // Se houver crianças cadastradas, seleciona a primeira (Lucas) por padrão
       if (this.children.length > 0) {
         this.selectedChild = this.children[0];
       }
